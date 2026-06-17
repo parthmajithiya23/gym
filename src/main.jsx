66534a34300da576a5afbdc0610f87e0b691b875
Parth <parthmajithiya386@gmail.com>
2026-06-17 11:57:40 +0530

    Clean main.jsx imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Navbar from './navbar'
-import Home from './home'
 
 
 createRoot(document.getElementById('root')).render(
